8ec1a65e5ddfd0c31eb9a3dbc802ad552e4488d5
Arnald Queiroga <arnaldqueiroga@gmail.com>
2020-06-17 22:49:43 -0300

    adicionando o desafio

---
+++ desafio1.js
@@ -0,0 +1,31 @@
+// const nome = "Carlos"
+// const peso = 84
+// const altura = 1.88
+
+// const imc = peso / (altura * altura);
+
+// if ( imc >= 28){
+//     console.log("Carlos, você está acima do peso")
+// } else (imc <= 29.9)
+// {
+//   console.log("Carlos, você não está acima do peso")
+// }
+
+/* =================================================
+Cálculo de Aposentadoria
+===================================================*/
+
+const nome = 'Silvana';
+const sexo = 'F';
+const idade = 48;
+const contribuicao = 23;
+
+tempoContribuicao = idade + contribuicao;
+
+if (tempoContribuicao >= 85) {
+  console.log('Silvana, você pode se aposentar!');
+} else {
+  console.log('Silvana, você ainda não pode se aposentar!');
+}
+
+// Terminado o desafio e pronto para ser comitado
