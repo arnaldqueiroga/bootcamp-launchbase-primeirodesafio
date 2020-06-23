cd0304790dfc594e7c6db25a887b8547e7cc462a
Arnald Queiroga <arnaldqueiroga@gmail.com>
2020-06-23 19:02:00 -0300

    Adicionando desafio 1_3

--- desafio1_3.js
+++ desafio1_3.js
@@ -1,16 +1,23 @@
-// Crie um programa que armazena um array de usuários (objetos), cada usuário tem um nome e suas tecnologias (novo array)
+const usuarios = [
+  { nome: 'Carlos', tecnologias: ['HTML', 'CSS'] },
+  { nome: 'Jasmine', tecnologias: ['JavaScript', 'CSS'] },
+  { nome: 'Tuane', tecnologias: ['HTML', 'Node.js'] },
+];
 
-  const usuarios = [
-    { nome: `Carlos`, tecnologias: [`HTML`, `CSS`] },
-    { nome: `Jasmine`, tecnologias: [`JavaScript`, `CSS`] },
-    { nome: `Tuane`, tecnologias: [`HTML`, `Node.js`] },
-  ];
 
-  for(let i=0; i < usuarios.length; i++){
-    console.log(`${usuarios[i].nome} trabalha com ${usuarios[i].tecnologias}`);
+function checaSeUsuarioUsaCSS(usuario) {
+  for (let tecnologia of usuario.tecnologias) {
+    if (tecnologia == `CSS`) return true;
   }
+  return false;
+}
 
+for (let i = 0; i < usuarios.length; i++){
+  const usuarioTrabalhaComCSS = checaSeUsuarioUsaCSS(usuarios[i]);
 
-
+  if  (usuarioTrabalhaComCSS){
+    console.log(`O usuário ${usuarios[i].nome} trabalha com CSS`);
+  }
+}
 
 
